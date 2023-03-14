e12182fb59403f4acfbb1fc54d1f1b956545a627
Blaine Huey <bhthenexttesla@gmail.com>
2023-03-14 13:31:58 -0400

    First draft of helper script to merge IR+RGB

---
+++ datacapture/merge_data.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import numpy as np
+
+from pathlib import Path
+import multiprocessing
+
+
+FLIR_PATH = "flir/"
+RGB_PATH = "rgb/"
+OUTPUT_PATH = "merged/"
+
+def merge_images(flir_file):
+    flir_name = flir_file.name
+    rgb_file = Path(RGB_PATH) / flir_name
+    
+    if rgb_file.exists():
+        flir_image = Image.open(flir_file)
+        rgb_image = Image.open(rgb_file)
+        
+        flir_image.load()
+        rgb_image.load()
+        
+        flir_array = np.asarray(flir_image)
+        rgb_array = np.asarray(rgb_image)
+
+        merged_array = np.concatenate((rgb_array, flir_array[:, :, np.newaxis]), axis=2)
+        merged_image = Image.fromarray(merged_array)
+        merged_image.save(Path(OUTPUT_PATH) / (flir_file.stem + ".png"), mode="RGBA")
+
+if __name__ == "__main__":
+    flir_files = Path(FLIR_PATH).glob("*")
+    
+    Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
+
+    tp = multiprocessing.Pool(multiprocessing.cpu_count())
+    
+    tp.map(merge_images, flir_files)
+
+    tp.close()
+    
